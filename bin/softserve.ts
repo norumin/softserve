e4f87d3c95b10b32633545fffdcfeb1af217c259
cednore <cednore@norumin.com>
2022-09-06 09:58:11 -0700

    ✨ vpc with two subnets

--- bin/softserve.ts
+++ bin/softserve.ts
@@ -1,21 +1,43 @@
 #!/usr/bin/env node
+
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import { App, Tags } from 'aws-cdk-lib';
+import capitalize = require('lodash.capitalize');
 import { SoftserveStack } from '../lib/softserve-stack';
 
-const app = new cdk.App();
-new SoftserveStack(app, 'SoftserveStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// load environment variables
+require('dotenv').config();
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+/**
+ * Configuration for the app.
+ */
+const config = {
+  stage: process.env.STAGE || 'production',
+  git_repo_url: process.env.GIT_REPO_URL || 'git://github.com/norumin/softserve.git',
+};
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+/**
+ * The app.
+ */
+const app = new App();
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// default tags of the app
+Tags.of(app).add('Stage', config.stage);
+Tags.of(app).add('Repo', config.git_repo_url);
+
+/**
+ * The stack.
+ */
+const stack = new SoftserveStack(
+  app,
+  `${capitalize(SoftserveStack.APP.toLowerCase())}${capitalize(config.stage.toLowerCase())}`,
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+  }
+);
+
+// default tags of the stack
+Tags.of(stack).add('App', SoftserveStack.APP);
--- lib/softserve-stack.ts
+++ lib/softserve-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class SoftserveStack extends cdk.Stack {
+  public static readonly APP = 'Softserve';
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SoftserveQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // vpc with 2 subnets
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: SoftserveStack.APP,
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
   }
 }
